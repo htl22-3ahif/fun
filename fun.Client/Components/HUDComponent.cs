dc226a0c54f60056a53720ba6165ddc1d619c0ae
Norbinator <nobytrem@gmail.com>
2016-01-14 17:27:03 +0100

    Implemented fps counter (buggy as hell)

--- fun.Client/Components/HUDComponent.cs
+++ fun.Client/Components/HUDComponent.cs
@@ -8,12 +8,16 @@ using System;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 using System.Drawing.Imaging;
+using fun.Client.Constructs;
 
 namespace fun.Client
 {
     internal sealed class HUDComponend : GameComponent
     {
         //Resources = http://www.opentk.com/doc/graphics/how-to-render-text-using-opengl
+
+        private Texture2D fpsTexture;
+        private Bitmap bitmap;
 
         private Font serif = new Font(FontFamily.GenericSerif, 24);
         private Font sans = new Font(FontFamily.GenericSansSerif, 24);
@@ -29,75 +33,36 @@ namespace fun.Client
 
         public override void Initialize()
         {
-            
+            bitmap = new Bitmap(Game.Width/4, Game.Height/4);
         }
 
         public override void Update(FrameEventArgs e)
         {
-            //Update TextDraw
             
         }
-    }
 
-    internal sealed class FPSRender
-    {
-        private GameWindow game;
-        public Bitmap text_bmp;
-        public Graphics gfx;
-        public int text_texture;
-        
-        public FPSRender(GameWindow game)
+        public override void Draw(FrameEventArgs e)
         {
-            this.game = game;
-            gfx = Graphics.FromImage(text_bmp);
-        }
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.Clear(Color.Transparent);
+                graphics.DrawString("FPS: " + Game.RenderFrequency, mono, Brushes.Tomato, new PointF(0, 0));
+            }
+            fpsTexture = new Texture2D(bitmap);
 
-        public void Initialize()
-        {
-            // Create Bitmap and OpenGL texture
-            text_bmp = new Bitmap(game.Width, game.Height); // match window size
-
-            text_texture = GL.GenTexture();
-            GL.BindTexture(TextureTarget.Texture2D, text_texture);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)All.Linear);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)All.Linear);
-            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, text_bmp.Width, text_bmp.Height, 0,
-                OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, IntPtr.Zero); // just allocate memory, so we can update efficiently using TexSubImage2D
-        }
-
-        public void Clear()
-        {
-            gfx.Clear(Color.Transparent);
-        }
-
-        public void DrawString(string text, Font font, Brush brush, PointF position)
-        {
-            gfx.DrawString(text, font, brush, position);
-        }
-
-        public void UploadBitmap()
-        {
-            BitmapData data = text_bmp.LockBits(new Rectangle(0, 0, text_bmp.Width, text_bmp.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, game.Width, game.Height, 0,
-                OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
-            text_bmp.UnlockBits(data);
-        }
-
-        public void RenderTexture()
-        {
             GL.MatrixMode(MatrixMode.Projection);
             GL.LoadIdentity();
-            GL.Ortho(0, game.Width, game.Height, 0, -1, 1);
+            GL.Ortho(0, Game.Width, Game.Height, 0, -1, 1);
 
             GL.Enable(EnableCap.Texture2D);
             GL.Enable(EnableCap.Blend);
-            GL.BlendFunc(BlendingFactorSrc.One, BlendingFactorDest.OneMinusSrcAlpha);
+            GL.BindTexture(TextureTarget.Texture2D, fpsTexture.ID);
 
             GL.Begin(PrimitiveType.Quads);
-            //GL.MultiTexCoord1(0f, 1f); GL.Vertex2(0f, 0f);
-            //GL.MultiTexCoord1(1f, 1f); GL.Vertex2(1f, 0f);
-            //GL.MultiTexCoord1(1f, 0f); GL.Vertex2(1f, 1f);
-            //GL.MultiTexCoord1(0f, 0f); GL.Vertex2(0f, 1f);
+            GL.TexCoord2(0f, 1f); GL.Vertex2(0f, 0f);
+            GL.TexCoord2(1f, 1f); GL.Vertex2(1f, 0f);
+            GL.TexCoord2(1f, 0f); GL.Vertex2(1f, 1f);
+            GL.TexCoord2(0f, 0f); GL.Vertex2(0f, 1f);
             GL.End();
         }
     }
--- fun.Client/Constructs/Texture2D.cs
+++ fun.Client/Constructs/Texture2D.cs
@@ -46,5 +46,28 @@ namespace fun.Client.Constructs
 
             GL.BindTexture(TextureTarget.Texture2D, 0);
         }
+
+        public Texture2D(Bitmap bitmap)
+            : this()
+        {
+            ID = GL.GenTexture();
+            GL.BindTexture(TextureTarget.Texture2D, ID);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+            Width = bitmap.Width;
+            Height = bitmap.Height;
+
+            var data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                System.Drawing.Imaging.ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba,
+                data.Width, data.Height, 0, PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+
+            bitmap.UnlockBits(data);
+
+            GL.BindTexture(TextureTarget.Texture2D, 0);
+        }
     }
 }
